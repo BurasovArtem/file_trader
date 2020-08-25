ebc1dbeaabafe54135e7bd21d86f8365de5d0b00
morty_smth <morti.show@gmail.com>
2020-08-25 19:33:15 +0500

    signin gmail

--- scripts/main.js
+++ scripts/main.js
@@ -103,3 +103,31 @@ function downloadFile(obj) {
 				console.log(error);
 			});
 }
+
+function signinGmail() {
+	var provider = new firebase.auth.GoogleAuthProvider();
+	firebase.auth().signInWithPopup(provider).then(function(result) {
+	  	var token = result.credential.accessToken;
+	  	var user = result.user;
+	  	console.log(result);
+	}).catch(function(error) {
+	  	var errorCode = error.code;
+	  	var errorMessage = error.message;
+	  	var email = error.email;
+	  	var credential = error.credential;
+	});
+}
+
+// function signinGithub() {
+// 	var provider = new firebase.auth.GithubAuthProvider();
+// 	firebase.auth().signInWithPopup(provider).then(function(result) {
+// 	  	var token = result.credential.accessToken;
+// 	  	var user = result.user;
+// 	  	console.log(result);
+// 	}).catch(function(error) {
+// 	  	var errorCode = error.code;
+// 	  	var errorMessage = error.message;
+// 	  	var email = error.email;
+// 	  	var credential = error.credential;
+// 	});
+// }
